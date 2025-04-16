20f55cac94920aabdaec68b2854e7dcdf5abd1bc
Mnhazul Islam Abir <minhazulislamabir5@gmail.com>
2025-04-17 00:18:39 +0600

    Add files via upload
    https://codeforces.com/contest/2094/problem/A

---
+++ Trippi_Troppi.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    string a, b, c;
+    for(int i = 0; i < n; i++){
+        cin >> a >> b >> c;
+        cout << a[0] << b[0] << c[0] << endl;
+    }
+    return 0;
+}
